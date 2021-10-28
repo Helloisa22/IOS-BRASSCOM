44c6443423cacb7a581c9c82b00de683cc28347e
Helloisa22 <helloisa.betriz44@gmail.com>
2021-10-28 01:46:52 -0300

    JavaScript - aula 22

---
+++ Javascript - aula 22  Professora/Exercicio Javascript/main.js
@@ -0,0 +1,21 @@
+function alertCookie () {
+    alert('Você quer um cookie');
+    console.log('Você quer um cookie')
+}
+
+const alertSucesso = () => {
+    alert('Mensagem gerada com sucesso');
+    console.log('Mensagem gerada com sucesso')
+}
+
+
+//Objeto
+const notebook = {
+    marca: "DELL",
+    modelo: "XPTO100",
+    cor: "Verde",
+    ano: 2020,
+    fabricante: "dell"
+}
+
+console.log(notebook);
