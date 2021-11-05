0c824f86cbe1b8c27e9ba42dd4a4ec832ce2862a
Helloisa22 <helloisa.betriz44@gmail.com>
2021-11-05 19:16:44 -0300

    Correção exercício aula 24

---
+++ JavaScript - aula 24/correcaoExercicioAula24.js
@@ -0,0 +1,24 @@
+function salarioColaborador(salario, aumento, novoSalario) {
+    if (salario <= 280) aumento = (salario * 20) / 100;
+    else if (salario > 280 && salario <= 700) aumento = (salario * 15) / 100;
+    else if (salario > 700 && salario <= 1500) aumento = (salario * 10) / 100;
+    else aumento = (salario * 5) / 100;
+
+    return (novoSalario = salario + aumento);
+}
+
+console.log(`Salario atual: R$${salarioColaborador()}`);
+
+console.clear();
+
+function maiorNumero() {
+    let num1 = 10;
+    let num2 = 20;
+    let num3 = 90;
+
+    if (num1 > num2) return `O numero ${num1} é maior`;
+    else if (num2 > num3) return 'O numero: ' + num2 + ' é maior';
+    else return `O numero ${num3} é maior`;
+}
+
+console.log(maiorNumero());
---
+++ JavaScript - aula 24/exercicioAula23.js
@@ -0,0 +1,51 @@
+const frutas1 = ['Joaquim', 'José', 'Silva', 'Xavier'];
+let a = frutas1;
+
+console.log(frutas1);
+
+//Inserir e Amanda
+//unshift
+const frutas2 = ['Joaquim', 'José', 'Silva', 'Xavier'];
+let w = frutas2.unshift('Amanda');
+let t = frutas2.pop(); // w = 3
+let k = frutas2.push('Zuleica');
+let o = frutas2.shift();
+//let v = delete frutas2[2];
+let y = frutas2.splice('silva');
+
+//numeros.splice(numeros.indexOf(3), 1);
+
+console.log(frutas2);
+
+// // Remover e Xavier
+// // pop()
+// const frutas4 = ['Joaquim', 'José', 'Silva', 'Xavier'];
+// let x = frutas4.pop(); // x = "Xavier saiu"
+
+// console.log(frutas4);
+
+// // Inserir e Zuleica
+// // push()
+// const frutas5 = ['Joaquim', 'José', 'Silva'];
+// let y = frutas5.push('Zuleica'); // y = Zuleica
+
+// console.log(frutas5);
+
+// Remover e Amanda
+//shift
+// const frutas3 = ['Amanda', 'Joaquim', 'José', 'Silva', 'Xavier'];
+// let z = frutas3.shift(); // z = 'Amanda'
+
+// console.log(frutas3);
+
+// // Remover Silva e inserir a Silvana
+// //delete
+// const frutas6 = ['Joaquim', 'José', 'Silva', 'Zuleica'];
+// delete frutas6[2];
+
+// console.log(frutas6);
+
+// const frutas7 = ['Joaquim', 'José', 'Silva', 'Zuleica'];
+//let p = frutas7.push('Silvana')
+
+// console.log(frutas7);
