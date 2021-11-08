b20fd82d85f0f8f2ea3ce6cdb34f2965eba5b7a5
Helloisa22 <helloisa.betriz44@gmail.com>
2021-11-08 18:36:23 -0300

    Subindo coisas

---
+++ JavaScript aula 28 - professora/JS/main.js
@@ -0,0 +1,18 @@
+class NomeClasse {
+    constructor() {}
+}
+
+class Carro {
+    constructor(nome, ano) {
+        this.nome = nome;
+        this.ano = ano;
+    }
+}
+
+// Alterar o título
+document.title = 123;
+// Criar um elemento <h1>
+let heading = document.createElement('H1');
+heading.innerHTML = 'Olá alunos!';
+document.body.appendChild(heading);
+cabecalho.style.borderBottom = 'solid 3px #000';
