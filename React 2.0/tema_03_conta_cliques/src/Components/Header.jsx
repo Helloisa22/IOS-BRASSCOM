b96485955cf70b59ffe80917799156cbefd413f9
Helloisa22 <helloisa.betriz44@gmail.com>
2021-11-20 00:12:10 -0300

    React parte 3

---
+++ React 2.0/tema_03_conta_cliques/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = (props) => {
+    return (
+        <div>
+            <h1>Olá, {props.nome}, seja bem-vindo!</h1>
+        </div>
+    );
+};
+
+Header.defaultProps ={
+    nome: "Jorel"
+}
+export default Header;
