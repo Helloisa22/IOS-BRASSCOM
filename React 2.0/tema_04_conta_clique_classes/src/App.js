8b8789a21802b4ea303c9fe723eef792a8871a3e
Helloisa22 <helloisa.betriz44@gmail.com>
2021-11-22 20:51:58 -0300

    Finalizado primeira parte do tema 04

---
+++ React 2.0/tema_04_conta_clique_classes/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Component } from 'react';
+import Exemplo from './Components/Exemplo';
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Exemplo />
+            </div>
+        );
+    }
+}
+
+export default App;
