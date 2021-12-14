d666db698be4c82e031a7180994f61be16ad58a1
Helloisa22 <helloisa.betriz44@gmail.com>
2021-12-13 21:26:23 -0300

    Revisão JS Parte 2

---
+++ DOM_Revisão/mainQuery.js
@@ -0,0 +1,19 @@
+// Metodo QuerySelector
+let paragrafo = document.querySelector('p');
+paragrafo.style.background = '#FBBEBA';
+
+// Acessando a tag e a classe
+let tagClasse = (document.querySelector('p.exemplo').innerHTML =
+    'Alterei essa classe para Nutella');
+
+// Acessando somente a classe
+let elementoClass = (document.querySelector('.myclass').style.fontSize =
+    'x-large');
+// elementoClass.style.fontSize = 'x-large';
+
+//Função chamadando pelo evento onclick()
+const Clique = () => {
+    document.querySelector('#my-span').style.backgroundColor = '#CEFBBA';
+};
+
+console.log(document.querySelectorAll('p'));
